6b91e00fc8b30449ee378641404dfc276758958f
Heng Jin En <hengh0305@gmail.com>
2023-10-17 17:55:15 +0800

    Flashcard css part don't know how to set it in the middle. Reuqest for help

--- study_app_client/src/components/flashcard/FlashcardContainer.js
+++ study_app_client/src/components/flashcard/FlashcardContainer.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Flashcard from './Flashcard';
 import './Flashcard.css';
 
 const flashcards = [
   { front: 'Question 1', back: 'Answer 1' },
+  { front: 'Question 2', back: 'Answer 2' },
+  { front: 'Question 3', back: 'Answer 3' },
   // Add more flashcards as needed
 ];
 
 const FlashcardContainer = () => {
+  const[currentCardIndex, setCurrentCardIndex] = useState(0);
+
+  const goToNextCard = () => {
+    if (currentCardIndex < flashcards.length - 1) {
+      setCurrentCardIndex(currentCardIndex + 1);
+    }
+  };
+
+  const goToPreviousCard = () => {
+    if (currentCardIndex > 0) {
+      setCurrentCardIndex(currentCardIndex - 1);
+    }
+  };
+
   return (
     <div className="flashcard-container">
-      {flashcards.map((card, index) => (
-        <Flashcard key={index} front={card.front} back={card.back} />
-      ))}
+      <Flashcard front={flashcards[currentCardIndex].front} back={flashcards[currentCardIndex].back} />
+
+      <div className="flashcard-navigation">
+        <button onClick={goToPreviousCard} disabled={currentCardIndex === 0}>
+          Previous
+        </button>
+        <button onClick={goToNextCard} disabled={currentCardIndex === flashcards.length - 1}>
+          Next
+        </button>
+      </div>
     </div>
   );
 };
