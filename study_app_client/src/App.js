0715092fac07ccb942ba190f982438181df5a70f
Heng Jin En <hengh0305@gmail.com>
2023-10-17 21:45:24 +0800

    Frontpage added. Request help for inserting the images.

--- study_app_client/src/App.js
+++ study_app_client/src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/sidebar/Navbar';
 import LoginPage from './components/login/LoginPage';
 import Profile from './components/profile/Profile';
 import Flashcard from './components/flashcard/FlashcardContainer';
+import Frotpage from "./components/frontpage/Frontpage"
 
 
 
@@ -18,6 +19,7 @@ function App() {
           <Route path="/" element={<Layout/>}>
             <Route path="/" element={<Navigate to="/login" replace />} />
             <Route path="/login" element={<LoginPage />} />
+            <Route path="/frontpage" element={<Frotpage />} />
             <Route path="/profile" element={<Profile />}></Route>
             <Route path="/flashcards" element={<Flashcard />}></Route>
             <Route path="*" element={<h1>Not Found</h1>}></Route>
