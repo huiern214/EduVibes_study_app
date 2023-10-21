2c93efd53730e7e05ef133afeacc75d1642c6139
Heng Jin En <hengh0305@gmail.com>
2023-10-21 11:29:04 +0800

    Flashcard slight amendments. Timetable done. Heal GPT corrected

--- study_app_client/src/components/flashcard/Timetable.js
+++ study_app_client/src/components/flashcard/Timetable.js
@@ -26,9 +26,23 @@ const Timetable = () => {
         // Add more cards as needed
     ];
 
+    const tableData = [
+        { day: '25-Sep', content: 'Sejarah Bab 1', '1 day': '', '2 days': '', '3 days': '', '7 days': '' },
+        { day: '26-Sep', content: 'Addmath Chap 2', '1 day': '1', '2 days': '', '3 days': '', '7 days': '' },
+        { day: '27-Sep', content: 'Biology Chap 3', '1 day': '2', '2 days': '1', '3 days': '', '7 days': '' },
+        { day: '28-Sep', content: 'Physics Chap 2', '1 day': '3', '2 days': '2', '3 days': '1', '7 days': '' },
+        // Add more rows as needed
+    ];
+
+
     const handleBack = () => {
         // Add your logic here
         alert('Back'); // You can replace this with your actual save logic
+    };
+
+    const handleAddNewContent = () => {
+        // Add your logic here
+        alert('Adding new content'); // You can replace this with your actual save logic
     };
 
     return (
@@ -55,8 +69,41 @@ const Timetable = () => {
                      <h2 className="card-title">{card.title}</h2>
                      {/* Add more card content here */}
                  </div>
-             ))}
+                )
+                )
+                }
             </div>
+            <div className='plan'>
+                <p className='plan-text'>Plan overview</p>
+            </div>
+            <table className="timetable-table">
+                <thead>
+                    <tr>
+                        <th>DAY</th>
+                        <th>Learning Content</th>
+                        <th>1 day</th>
+                        <th>2 days</th>
+                        <th>3 days</th>
+                        <th>7 days</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tableData.map((row, index) => (
+                        <tr key={index}>
+                            <td>{row.day}</td>
+                            <td>{row.content}</td>
+                            <td>{row['1 day']}</td>
+                            <td>{row['2 days']}</td>
+                            <td>{row['3 days']}</td>
+                            <td>{row['7 days']}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <button className="add-button" onClick={handleAddNewContent}>
+                Add Content
+            </button>
+            <div className='section'></div>
         </div>
     );
 }
